59887d06e4708dc9534c5ca56b0ca899616d86a5
acuzzio <acuzzio@gmail.com>
2013-12-04 11:54:28 +0100

    chunk files

--- parallelProject/src/CreateInfo.hs
+++ parallelProject/src/CreateInfo.hs
@@ -30,10 +30,15 @@ createInfo = do
        mapM_ (genInfoFile chargeTrFragment) outputs
    
 createInfoP = do
-   outs <- readShell "ls */*.out"
-   let outputs = lines outs
-   pids <- mapM (\x -> async $genInfoFile chargeTrFragment x) outputs
-   mapM_ wait pids
+       outs <- readShell "ls outs/*.out"
+       let outputs = lines outs
+           chunks  = chunksOf 10 outputs
+       sequence_ $ processFiles `fmap` chunks
+
+processFiles :: [FilePath] -> IO ()
+processFiles outputs = do
+       pids <- mapM (\x -> async $genInfoFile chargeTrFragment x) outputs
+       mapM_ wait pids
 
 rdInfoFile  :: String -> IO(Dinamica)
 rdInfoFile fn = do
