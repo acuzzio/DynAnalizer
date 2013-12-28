8e0d31d18e6b4968595448090847b778a4f90a77
acuzzio <acuzzio@gmail.com>
2013-12-28 11:29:14 +0100

    Massimo separated

--- CalculateDAT.hs
+++ CalculateDAT.hs
@@ -1,3 +1,5 @@
+module CalculateDAT where
+
 import System.ShQQ
 import Text.Printf
 import Data.List.Split
@@ -25,7 +27,7 @@ data DinamicV = DinamicV {
          ,getBlaV        :: [String]
           } deriving Show
 
-main = do 
+createDATAs = do 
      outs <- readShell $ "ls " ++ folder ++ "/*.info"
      let outputs = lines outs
      mapM_ (createDATA betaList ccccList) outputs
@@ -163,54 +165,6 @@ blaPSB3 blaList list = let
         res             = singles - doubles
         in res
 
-reportMassimo :: IO()
-reportMassimo = do
-    stringZ <- readerData 
-    let checkLN     = zip [0..] $ map length stringZ
-        filtered    = unwords $ map (show . fst) $ filter (\x -> snd x < 200) checkLN
-        messaGe     = if filtered == "" then "Everything OK" else "Check out short trajectories: " ++ filtered
-    putStrLn messaGe
-    let writeF x    = intercalate "  \n"$ map unlines $ map (map unwords) x
-        avgDihedral = map (map (\a -> read (a!!3) :: Double)) $ map (filter (\x -> x!!8=="S1")) $ transpose stringZ
-        avg xs   = (sum xs)/(fromIntegral $ length xs)
-        avgZip   = zip [1..] $ map avg avgDihedral -- steps starts from 1
-        form (a,b) = [show a,show b]
-        ccccAVG  = unlines $ take 100 $ map unwords $ map form avgZip
-    writeFile "CCCC" $ writeF stringZ
-    writeFile "CCCCS1AVG" ccccAVG
-    let hopOrNot    = map (all (\x -> x /= "10")) $ map (map (\x-> x!!9)) stringZ 
-        isomYorN xs = map (map (\a -> read (a!!3) :: Double)) $ map (stringZ !!) xs
-        counter x   = if isomCond x then 1 else 0
-        whoNotHop   = map fst $ filter (\x-> snd x == True) $ zip [0..] hopOrNot
-        notHopIsoC  = sum $ map counter $ map last $ isomYorN whoNotHop
-        notHopnIsoC = (length whoNotHop) - notHopIsoC
-        whoHop      = map fst $ filter (\x-> snd x == False) $ zip [0..] hopOrNot
-        whoHopAndIs = map fst $ filter (\x-> isomCond $ snd x) $ zip whoHop $ map last $ isomYorN whoHop
-        whoHopAndNo = map fst $ filter (\x-> not . isomCond $ snd x ) $ zip whoHop $ map last $ isomYorN whoHop
-        hopIsoC     = sum $ map counter $ map last $ isomYorN whoHop     
-        hopNIsoC    = (length whoHop) - hopIsoC
-        aveRAGES    = map (\x -> averageSublist stringZ x 3 200) [whoNotHop,whoHopAndIs,whoHopAndNo] -- 3 is cccc, 200 is first 200 substeps
-        printZ x    = (printf "%.2f" x) :: String
-    writeFile "CCCCAVERAGES" $ unlines $ map unwords $ map (map printZ) $ transpose aveRAGES
-    putStrLn $ "Hop and Iso -> " ++ (show hopIsoC)
-    putStrLn $ "Hop not Iso -> " ++ (show hopNIsoC)
-    putStrLn $ "NoHop and Iso -> " ++ (show notHopIsoC)
-    putStrLn $ "NoHop not Iso -> " ++ (show notHopnIsoC)
-    let total = hopNIsoC + hopIsoC + notHopnIsoC + notHopIsoC
-    putStrLn $ "Total -> " ++ (show total)
-    let rateHOP = (fromIntegral (hopIsoC * 100) / (fromIntegral (hopIsoC+hopNIsoC))) :: Double
-    putStrLn $ "only Hopped Iso/notIso -> " ++ (printZ rateHOP) ++ "%"
-    let rateNOH = (fromIntegral (notHopIsoC * 100) / (fromIntegral (notHopIsoC+notHopnIsoC))) :: Double 
-    putStrLn $ "only NON Hopped Iso/notIso -> " ++ (printZ rateNOH) ++ "%"
-    let rateTOT = (fromIntegral (hopIsoC * 100) / fromIntegral (total)) :: Double
-    putStrLn $ "Total Iso/notIso -> " ++ (printZ rateTOT) ++ "%"
-    hopS
-    mapped stringZ 3 "CCCCDensity"
-    chargeTmap stringZ "TOT" [0.4,0.5,0.6]
-    let sZeroOnly = map (filter (\x -> x!!8 == "S0")) stringZ
-    chargeTmap sZeroOnly "S0" [0.4,0.5,0.6]
-
-
 averageSublist :: [[[String]]] -> [Int] -> Int -> Int -> [Double]
 averageSublist stringOne trajxs index thres = let
         rightTrajectories = map (stringOne !!) trajxs
@@ -263,5 +217,35 @@ hopS = do
       getHOPs     = map getHOP $ allJumps
   mapM_ (\x -> writeFile ("CCCCHOP" ++ fst x) $ writeF (getHOPs !! snd x)) $ zip allJumps [0..]
 
+whoIsomerize :: IO()
+whoIsomerize = do
+    stringZ <- readerData 
+    let checkLN     = zip [0..] $ map length stringZ
+        filtered    = unwords $ map (show . fst) $ filter (\x -> snd x < 200) checkLN
+        messaGe     = if filtered == "" then "Everything OK" else "Check out short trajectories: " ++ filtered
+    putStrLn messaGe
+    let hopOrNot    = map (all (\x -> x /= "10")) $ map (map (\x-> x!!9)) stringZ 
+        isomYorN xs = map (map (\a -> read (a!!3) :: Double)) $ map (stringZ !!) xs
+        counter x   = if isomCond x then 1 else 0
+        whoNotHop   = map fst $ filter (\x-> snd x == True) $ zip [0..] hopOrNot
+        notHopIsoC  = sum $ map counter $ map last $ isomYorN whoNotHop
+        notHopnIsoC = (length whoNotHop) - notHopIsoC
+        whoHop      = map fst $ filter (\x-> snd x == False) $ zip [0..] hopOrNot
+        whoHopAndIs = map fst $ filter (\x-> isomCond $ snd x) $ zip whoHop $ map last $ isomYorN whoHop
+        whoHopAndNo = map fst $ filter (\x-> not . isomCond $ snd x ) $ zip whoHop $ map last $ isomYorN whoHop
+        hopIsoC     = sum $ map counter $ map last $ isomYorN whoHop     
+        hopNIsoC    = (length whoHop) - hopIsoC
+    putStrLn $ "Hop and Iso : " ++ (show hopIsoC)
+    putStrLn $ "Hop not Iso : " ++ (show hopNIsoC)
+    putStrLn $ "NoHop and Iso : " ++ (show notHopIsoC)
+    putStrLn $ "NoHop not Iso : " ++ (show notHopnIsoC)
+    let total = hopNIsoC + hopIsoC + notHopnIsoC + notHopIsoC
+    putStrLn $ "Total : " ++ (show total)
+    let rateHOP = (fromIntegral (hopIsoC * 100) / (fromIntegral (hopIsoC+hopNIsoC))) :: Double
+    putStrLn $ "only Hopped Iso/notIso : " ++ (printZ rateHOP) ++ "%"
+    let rateNOH = (fromIntegral (notHopIsoC * 100) / (fromIntegral (notHopIsoC+notHopnIsoC))) :: Double 
+    putStrLn $ "only NON Hopped Iso/notIso : " ++ (printZ rateNOH) ++ "%"
+    let rateTOT = (fromIntegral (hopIsoC * 100) / fromIntegral (total)) :: Double
+    putStrLn $ "Total Iso/notIso : " ++ (printZ rateTOT) ++ "%"
 
 
--- Functions.hs
+++ Functions.hs
@@ -1,9 +1,13 @@
 module Functions where
+
 import Data.List.Split
 import Data.List
+import Text.Printf
 
 read2 :: String -> Double
 read2 x = read x :: Double
 
 writeF :: [[[String]]] -> String
 writeF x  = intercalate "  \n"$ map unlines $ map (map unwords) x
+
+printZ x    = (printf "%.2f" x) :: String
