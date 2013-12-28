2fde2be93c7ee98849af454120b05c1ce4ea749f
acuzzio <acuzzio@gmail.com>
2013-12-28 14:26:28 +0100

    now with BAD gnuplotter

--- GnuplotZ.hs
+++ GnuplotZ.hs
@@ -3,21 +3,26 @@ module GnuplotZ where
 import System.ShQQ
 import System.Process
 import Data.List
+import Data.List.Split
 import Control.Monad
 import Text.Parsec
 import Text.ParserCombinators.Parsec (parseFromFile)
 import Data.Functor.Identity
 
 import CreateInfo
+import IntCoor
 import Inputs
+import Functions
 
 data PlotType = Pop | Ene | Dyn deriving (Eq,Show)
 
-energiesPopulation = do
+plotEnergiesPopulation :: IO ()
+plotEnergiesPopulation = do
        a <- readShell $ "ls " ++ folder ++ "/*.info"
        let files = lines a
        mapM_ graficami files
 
+graficami :: FilePath -> IO ()
 graficami file = do
    dina <- rdInfoFile file
    let popEne = getEnergies dina
@@ -26,7 +31,7 @@ graficami file = do
    writeGnuplots dt rlxRoot file popEne 
    system "gnuplot < gnuplotScript"
    system "rm sdafrffile* gnuplotScript"
-   print "done"
+   putStrLn $ file ++ ": done"
 
 writeGnuplots :: Double -> Int -> FilePath -> [[Double]] -> IO()
 writeGnuplots dt rlxRoot file xss = do
@@ -70,8 +75,7 @@ createPlotLine (Ene,c,d) b dt = "\"" ++ b ++ "\"" ++ " u ($0*" ++ (fromAUtoFemto
 createPlotLine (Dyn,c,d) b dt = "\"" ++ b ++ "\"" ++ " u ($0*" ++ (fromAUtoFemtoDT dt) ++ "):1 w lp ps 1 linecolor rgb \"black\" t \"RlxRoot\""
 
 fromAUtoFemtoDT :: String -> String
-fromAUtoFemtoDT dt = let read2 x = read x :: Double
-                     in show ((read2 dt) / 41.34144728138643)
+fromAUtoFemtoDT dt = show ((read2 dt) / 41.34144728138643)
 
 findRlxRT a = let len           = length a
                   states        = div (len-1) 2
@@ -79,3 +83,45 @@ findRlxRT a = let len           = length a
                   c             = zip [0..] initA
                   currentState  = fst . head $ filter (\x -> (head $ snd x) == (head tailA)) c
               in (currentState - states)
+
+plotBondAngleDihedrals :: [Int] -> IO()
+plotBondAngleDihedrals xs = do
+   a <- readShell $ "ls " ++ folder ++ "/*.info"
+   let files = lines a        
+   mapM_ (\x -> pBAD x xs) files
+
+pBAD :: FilePath -> [Int] -> IO()
+pBAD fn xs = do
+   case length xs of
+        2         -> extractBAD fn xs bond "Bond"
+        3         -> extractBAD fn xs angle "Angle"
+        4         -> extractBAD fn xs dihedral "Dihedral"
+        otherwise -> putStrLn errorMsg
+
+errorMsg = "a list of 2 -> bond\na list of 3 -> angle\na list of 4 -> dihedral"
+
+fn = "e-Samer/geom000.info"
+list = [1,2] :: [Int]
+
+extractBAD :: FilePath -> [Int] -> ([Vec Double] -> Double) -> String -> IO () 
+extractBAD fn atomL fun label = do 
+  dina <- rdInfoFile fn
+  let coord  = getCoordinates dina
+      dt     = getDT dina
+      atomN  = getAtomN dina
+      atomLI = map pred atomL
+      coordS = chunksOf atomN coord
+      fileN  = takeWhile (/= '.') fn
+      values = map fun $ map (\x -> map ( x !!) atomLI) coordS
+      smLab  = zipWith (++) (getAtomT dina) (map show [1..])
+      rightL = label ++ " " ++ (unwords $ map (smLab!!) atomLI)
+      fnLabe = fileN ++ (filter (/= ' ') rightL) 
+      header = "set title \"" ++ rightL ++ "\"\nset xlabel \"fs\"\nset key off\nset format y \"%6.2f\"\nset output '" ++ fileN ++ (filter (/= ' ') rightL) ++ ".png'\nset terminal pngcairo size 1224,830 enhanced font \", 12\"\nplot \"" ++ (fnLabe ++ "GnupValues") ++ "\" u ($0*" ++ (fromAUtoFemtoDT (show dt)) ++ "):1 w lines"
+  writeFile (fnLabe ++ "gnuplotScript") header
+  writeFile (fnLabe ++ "GnupValues") $ unlines $ map show values
+  system $ "gnuplot < " ++ (fnLabe ++ "gnuplotScript")
+  system $ "rm " ++ (fnLabe ++ "GnupValues") ++ " " ++ (fnLabe ++ "gnuplotScript")
+  putStrLn $ fileN ++ ": done"
+
+
+
