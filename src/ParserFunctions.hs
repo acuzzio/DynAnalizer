9b1bbc24aa67496e736fc973e5a70d69118fda34
Massimo Zaniboni <massimo.zaniboni@asterisell.com>
2016-02-23 19:16:42 +0100

    Proposed change to the slow parsing function, but not tested or compiled.

--- src/ParserFunctions.hs
+++ src/ParserFunctions.hs
@@ -50,19 +50,58 @@ trimDoubleSpaces = B.unwords . B.words
 
 treatTriplets = B.init . B.unlines . map trimDoubleSpaces 
 
+-- | Throw away everything until the string pattern
+--   TODO nota ho aggiunto qua il parametro ignoreCase tanto e` un test veloce che non rallenta niente
+--
+skipTill :: Bool -> B.ByteString -> Parser ()
+skipTill ignoreCase pattern = consume patternStart
+  
+  where
+
+    patternStart = B.uncons pattern
+    -- NOTE: B.uncons is a fast operation on bytestrings and it does not involve any copy.
+    -- TODO in case this is not the case, we can change the function argument to `[Char8]` and use list operations. I'm saying this because probably the argument is always specified from the user of the function, and it can be sent in list format without any problem.
+
+    consume Nothing = do
+      return ()
+      -- we parsed the complete `pattern` string, and so we can stop
+
+    consume (Just (ch1, rs)) = do
+      nch <- peekChar
+      -- NOTE: retrieve the current character, but without consuming it
+
+      case nch of
+        Nothing
+          -> return ()
+             -- reached end of input
+
+        Just ch2
+          -> case (ch1 == ch2) || (ignoreCase && (B.toLower ch1 == B.toLower ch2)) of
+               True
+                 -> do anyChar
+                       consume (B.uncons rs)
+                       -- we found the next valid character of the `pattern` string,
+                       -- so we can continue parsing input searching for the next valid character
+               False
+                 -> do anyChar
+                       consume patternStart
+                       -- the character of the input is not valid, so we continue parsing it,
+                       -- searching again the `pattern` string from the beginning.
+
 -- throw away everything until the string pattern
-skipTill :: B.ByteString -> Parser ()
-skipTill pattern = skipWhile funL *> ( (string pattern *> pure () )  <|> (anyChar *> skipTill pattern))
- 
+skipTill_slow :: B.ByteString -> Parser ()
+skipTill_slow pattern = skipWhile funL *> ( (string pattern *> pure () )  <|> (anyChar *> skipTill pattern))
+
   where funL = (/= l)
         l    = B.head pattern
- 
+
 -- throw away everything until the string pattern CASE UNSEnSiTiVE
+-- TODO questa potresti cancellarla e chiamara la versione generica con il parametro ignoreCase
 skipTillCase :: B.ByteString -> Parser ()
 skipTillCase pattern = skipWhile (\x -> condition l x) *> ( (stringCI pattern *> pure () )  <|> (anyChar *> skipTillCase pattern))
- 
+
   where l = firstLetterU pattern
- 
+
 -- throw away everything until patternRight is found. In case patternWrong is found I will fail/return error/return a certain string with the parser
 skipTillSafe :: B.ByteString -> B.ByteString -> Parser ()
 skipTillSafe patternRight patternWrong =  skipWhile condition2 *> controlLetter
